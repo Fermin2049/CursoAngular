536448881df90c5fcfe62a1a002381e4709f5721
Fermin2049 <fermin2049@gmail.com>
2025-10-15 19:33:20 -0300

    debounce=Busqueda automatica

--- 04-contry-app/src/app/country/components/search-input/search-input.component.ts
+++ 04-contry-app/src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -8,4 +8,16 @@ import { Component, input, output } from '@angular/core';
 export class SearchInputComponent {
   placeholder = input('Buscar');
   value = output<string>();
+
+  imputValue = signal<string>('');
+
+  debonceEffect = effect((onClenup) => {
+    const value = this.imputValue();
+
+    const timeot = setTimeout(() => {
+      this.value.emit(value);
+    }, 500);
+
+    onClenup(() => clearTimeout(timeot));
+  });
 }
